11e9acafa80f62f6878ef0b10384114be6d14b53
Kumaravel V <75235334+Kumaravel655@users.noreply.github.com>
2025-07-17 18:34:51 +0530

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
+from .forms import UserSignupForm
+from .models import Users, Roles
 
-<<<<<<< HEAD
 def index_view(request):
     return render(request, 'index.html')
 
@@ -9,13 +13,48 @@ def role_choice_view(request):
 
 def login_page(request):
     return render(request, 'loginpage.html')
-=======
-def profile_view(request):
-    return render(request, 'profile.html')
 
-def dept_view(request):
-    return render(request, 'dept.html')
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserSignupForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data['password'])
 
-def update_view(request):
-    return render(request, 'update.html')
->>>>>>> cece332 (changes)
+            # Set default role
+            default_role = Roles.objects.filter(role_name='User').first()
+            user.role = default_role
+
+            user.status = "Active"
+            user.save()
+            return redirect('login')  # or any success page
+    else:
+        form = UserSignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+
+def signin_view(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            user_obj = Users.objects.get(email=email)
+            username = user_obj.username
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                return render(request, "signin.html", {"error": "Invalid credentials"})
+        except Users.DoesNotExist:
+            return render(request, "signin.html", {"error": "User not found"})
+
+    return render(request, "signin.html")
+
+def dashboard_view(request):
+    if not request.user.is_authenticated:
+        return redirect('signin')
+    return render(request, "dashboard.html")
+  
